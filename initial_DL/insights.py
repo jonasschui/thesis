2b2f69c26caf69c4ef1a7be81ec174f1d61201b8
unknown <j.j.a.schuitemaker@gmail.com>
2019-04-23 14:33:53 +0200

    added function to check important features such as bigrams for the initial decision list

--- initial_DL/insights.py
+++ initial_DL/insights.py
@@ -1,7 +1,21 @@
 from collections import Counter
 
+def get_unique_ne(ne_list, othercat_lists):
+	other = set([j for i in othercat_lists for j in i])
+	unique_list = []
+	for item in ne_list:
+		if item in other:
+			continue
+		else:
+			unique_list.append(item)
+	return unique_list
+	
+	
+	
+
 def main():
 	infile = open("../data/SoNaR1_training.txt", "r",encoding="utf8")
+	next(infile)
 	main_cats = ["PER", "LOC" , "ORG" , "PRO", "EVE", "MISC"]
 	per = []
 	loc = []
@@ -9,8 +23,10 @@ def main():
 	eve = []
 	pro = []
 	misc = []
+	lines = []
 	for line in infile:
-		line = line.rstrip().split()
+		line = line.rstrip().split('\t')
+		lines.append(line)
 		try:
 			if line[-2] == "PER":
 				per.append(line)
@@ -26,6 +42,8 @@ def main():
 				misc.append(line)
 		except:
 			continue
+	org_subcats = ['NONE', 'MISC', 'GOV', 'COM']
+	# LOCATION DATA
 	location_subcats = ['PUNT', 'LIJN', 'BC', 'WATER', 'NONE', 'REGIO', 'FICTIEF', 'LAND', 'CONT', 'HEELAL']
 	punt=lijn=bc=water=none=regio=fictief=land=cont=heelal = 0
 	puntNE= []
@@ -40,7 +58,7 @@ def main():
 	heelalNE = []
 	for item in loc:
 		lsubcat = item[-1]
-		ne = ' '.join(item[2:-2]).lower().rstrip()
+		ne = item[2]
 		if lsubcat == location_subcats[0]:
 			punt += 1
 			puntNE.append(ne)
@@ -71,15 +89,298 @@ def main():
 		elif lsubcat == location_subcats[9]:
 			heelal += 1
 			heelalNE.append(ne)
-		
+			
+	# GET UNIQUE LISTS OF WORDS PER SUBCAT
+	uniquepunt = get_unique_ne(puntNE, [lijnNE, bcNE,waterNE,noneNE,regioNE,fictiefNE,landNE,contNE,heelalNE])
+	uniquelijn = get_unique_ne(lijnNE, [puntNE, bcNE,waterNE,noneNE,regioNE,fictiefNE,landNE,contNE,heelalNE])
+	uniquebc = get_unique_ne(bcNE, [lijnNE, puntNE,waterNE,noneNE,regioNE,fictiefNE,landNE,contNE,heelalNE])
+	uniquewater = get_unique_ne(waterNE, [lijnNE, bcNE,puntNE,noneNE,regioNE,fictiefNE,landNE,contNE,heelalNE])
+	uniquenone = get_unique_ne(noneNE, [lijnNE, bcNE,waterNE,puntNE,regioNE,fictiefNE,landNE,contNE,heelalNE])
+	uniqueregio = get_unique_ne(regioNE, [lijnNE, bcNE,waterNE,noneNE,puntNE,fictiefNE,landNE,contNE,heelalNE])
+	uniquefictief = get_unique_ne(fictiefNE, [lijnNE, bcNE,waterNE,noneNE,regioNE,puntNE,landNE,contNE,heelalNE])
+	uniqueland = get_unique_ne(landNE, [lijnNE, bcNE,waterNE,noneNE,regioNE,fictiefNE,puntNE,contNE,heelalNE])
+	uniquecont = get_unique_ne(contNE, [lijnNE, bcNE,waterNE,noneNE,regioNE,fictiefNE,landNE,puntNE,heelalNE])
+	uniqueheelal = get_unique_ne(heelalNE, [lijnNE, bcNE,waterNE,noneNE,regioNE,fictiefNE,landNE,contNE,puntNE])
+
+	# GET LIST OF UNIQUE BIGRAMS PER SUBCAT
+	
+	location_subcats = ['PUNT', 'LIJN', 'BC', 'WATER', 'NONE', 'REGIO', 'FICTIEF', 'LAND', 'CONT', 'HEELAL']
+	
+	punt_pre_bi = []
+	lijn_pre_bi = []
+	bc_pre_bi = []
+	water_pre_bi = []
+	none_pre_bi = []
+	regio_pre_bi = []
+	fictief_pre_bi = []
+	land_pre_bi = []
+	cont_pre_bi = []
+	heelal_pre_bi = []
+	
+	punt_post_bi = []
+	lijn_post_bi = []
+	bc_post_bi = []
+	water_post_bi = []
+	none_post_bi = []
+	regio_post_bi = []
+	fictief_post_bi = []
+	land_post_bi = []
+	cont_post_bi = []
+	heelal_post_bi = []
+	
 	org_subcats = ['NONE', 'MISC', 'GOV', 'COM']
+	
+	org_none_pre_bi = []
+	org_misc_pre_bi = []
+	gov_pre_bi = []
+	com_pre_bi = []
+	
+	org_none_post_bi = []
+	org_misc_post_bi = []
+	gov_post_bi = []
+	com_post_bi = []
+	
+	
+	lines_for_bigrams = []
+	for line in lines:
+		if line[0] not in ["DOCUMENT", "SENTENCE"]:
+			lines_for_bigrams.append(line)
+		elif line[0] == "SENTENCE":
+			lines_for_bigrams.append(["fakeID", "fakeMarkable","XXXXXXXX"])
+		else:
+			continue
+	# get list of all word bigrams per subcat
+	for i in range(len(lines_for_bigrams)):
+		if i < (len(lines_for_bigrams) - 2):
+			word1 = lines_for_bigrams[i][2]
+			word2 = lines_for_bigrams[(i+1)][2]
+			word3 = lines_for_bigrams[(i+2)][2]
+			if len(lines_for_bigrams[(i+2)]) == 5:
+				cat = lines_for_bigrams[(i+2)][-2]
+				subcat = lines_for_bigrams[(i+2)][-1]
+				# PRE NE WORDS
+				if cat == "LOC":
+					if subcat == location_subcats[0]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							punt_pre_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							punt_pre_bi.append(tuple((word1, word2)))
+					elif subcat == location_subcats[1]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							lijn_pre_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							lijn_pre_bi.append(tuple((word1, word2)))
+					elif subcat == location_subcats[2]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							bc_pre_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							bc_pre_bi.append(tuple((word1, word2)))
+					elif subcat == location_subcats[3]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							water_pre_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							water_pre_bi.append(tuple((word1, word2)))
+					elif subcat == location_subcats[4]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							none_pre_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							none_pre_bi.append(tuple((word1, word2)))
+					elif subcat == location_subcats[5]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							regio_pre_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							regio_pre_bi.append(tuple((word1, word2)))
+					elif subcat == location_subcats[6]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							fictief_pre_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							fictief_pre_bi.append(tuple((word1, word2)))
+					elif subcat == location_subcats[7]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							land_pre_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							land_pre_bi.append(tuple((word1, word2)))
+					elif subcat == location_subcats[8]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							cont_pre_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							cont_pre_bi.append(tuple((word1, word2)))
+					elif subcat == location_subcats[9]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							heelal_pre_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							heelal_pre_bi.append(tuple((word1, word2)))
+				if cat == "ORG":
+					if subcat == org_subcats[0]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							org_none_pre_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							org_none_pre_bi.append(tuple((word1, word2)))
+					elif subcat == org_subcats[1]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							org_misc_pre_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							org_misc_pre_bi.append(tuple((word1, word2)))
+					elif subcat == org_subcats[2]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							gov_pre_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							gov_pre_bi.append(tuple((word1, word2)))
+					elif subcat == org_subcats[3]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							com_pre_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							com_pre_bi.append(tuple((word1, word2)))
+			# POST NE WORDS
+			if len(lines_for_bigrams[(i)]) == 5:
+				cat = lines_for_bigrams[(i)][-2]
+				subcat = lines_for_bigrams[(i)][-1]
+				if cat == "LOC":
+					if subcat == location_subcats[0]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							punt_post_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							punt_post_bi.append(tuple((word2, word3)))
+					elif subcat == location_subcats[1]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							lijn_post_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							lijn_post_bi.append(tuple((word2, word3)))
+					elif subcat == location_subcats[2]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							bc_post_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							bc_post_bi.append(tuple((word2, word3)))
+					elif subcat == location_subcats[3]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							water_post_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							water_post_bi.append(tuple((word2, word3)))
+					elif subcat == location_subcats[4]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							none_post_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							none_post_bi.append(tuple((word2, word3)))
+					elif subcat == location_subcats[5]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							regio_post_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							regio_post_bi.append(tuple((word2, word3)))
+					elif subcat == location_subcats[6]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							fictief_post_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							fictief_post_bi.append(tuple((word2, word3)))
+					elif subcat == location_subcats[7]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							land_post_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							land_post_bi.append(tuple((word2, word3)))
+					elif subcat == location_subcats[8]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							cont_post_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							cont_post_bi.append(tuple((word2, word3)))
+					elif subcat == location_subcats[9]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							heelal_post_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							heelal_post_bi.append(tuple((word2, word3)))
+				if cat == "ORG":
+					if subcat == org_subcats[0]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							org_none_post_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							org_none_post_bi.append(tuple((word2, word3)))
+					elif subcat == org_subcats[1]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							org_misc_post_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							org_misc_post_bi.append(tuple((word2, word3)))
+					elif subcat == org_subcats[2]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							gov_post_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							gov_post_bi.append(tuple((word2, word3)))
+					elif subcat == org_subcats[3]:
+						# first word in new sentence happens to be a NE
+						if word2 == "XXXXXXXX":
+							com_post_bi.append(tuple(("XXXXXXXX", "XXXXXXXX")))
+						else:
+							com_post_bi.append(tuple((word2, word3)))
+	
+	
+	
+	# set bigram lists to be unique
+	punt_pre_bi_un = get_unique_ne(punt_pre_bi, [lijn_pre_bi,bc_pre_bi,water_pre_bi,none_pre_bi,regio_pre_bi,fictief_pre_bi,land_pre_bi,cont_pre_bi,heelal_pre_bi])
+	lijn_pre_bi_un = get_unique_ne(lijn_pre_bi, [punt_pre_bi,bc_pre_bi,water_pre_bi,none_pre_bi,regio_pre_bi,fictief_pre_bi,land_pre_bi,cont_pre_bi,heelal_pre_bi])
+	bc_pre_bi_un = get_unique_ne(bc_pre_bi, [lijn_pre_bi,punt_pre_bi,water_pre_bi,none_pre_bi,regio_pre_bi,fictief_pre_bi,land_pre_bi,cont_pre_bi,heelal_pre_bi])
+	water_pre_bi_un = get_unique_ne(water_pre_bi, [lijn_pre_bi,bc_pre_bi,punt_pre_bi,none_pre_bi,regio_pre_bi,fictief_pre_bi,land_pre_bi,cont_pre_bi,heelal_pre_bi])
+	none_pre_bi_un = get_unique_ne(none_pre_bi, [lijn_pre_bi,bc_pre_bi,water_pre_bi,punt_pre_bi,regio_pre_bi,fictief_pre_bi,land_pre_bi,cont_pre_bi,heelal_pre_bi])
+	regio_pre_bi_un = get_unique_ne(regio_pre_bi, [lijn_pre_bi,bc_pre_bi,water_pre_bi,none_pre_bi,punt_pre_bi,fictief_pre_bi,land_pre_bi,cont_pre_bi,heelal_pre_bi])
+	fictief_pre_bi_un = get_unique_ne(fictief_pre_bi, [lijn_pre_bi,bc_pre_bi,water_pre_bi,none_pre_bi,regio_pre_bi,punt_pre_bi,land_pre_bi,cont_pre_bi,heelal_pre_bi])
+	land_pre_bi_un = get_unique_ne(land_pre_bi, [lijn_pre_bi,bc_pre_bi,water_pre_bi,none_pre_bi,regio_pre_bi,fictief_pre_bi,punt_pre_bi,cont_pre_bi,heelal_pre_bi])
+	cont_pre_bi_un = get_unique_ne(cont_pre_bi, [lijn_pre_bi,bc_pre_bi,water_pre_bi,none_pre_bi,regio_pre_bi,fictief_pre_bi,land_pre_bi,punt_pre_bi,heelal_pre_bi])
+	heelal_pre_bi_un = get_unique_ne(heelal_pre_bi, [lijn_pre_bi,bc_pre_bi,water_pre_bi,none_pre_bi,regio_pre_bi,fictief_pre_bi,land_pre_bi,cont_pre_bi,punt_pre_bi])
+	
+	punt_post_bi_un = get_unique_ne(punt_post_bi, [lijn_post_bi,bc_post_bi,water_post_bi,none_post_bi,regio_post_bi,fictief_post_bi,land_post_bi,cont_post_bi,heelal_post_bi])
+	lijn_post_bi_un = get_unique_ne(lijn_post_bi, [punt_post_bi,bc_post_bi,water_post_bi,none_post_bi,regio_post_bi,fictief_post_bi,land_post_bi,cont_post_bi,heelal_post_bi])
+	bc_post_bi_un = get_unique_ne(bc_post_bi, [lijn_post_bi,punt_post_bi,water_post_bi,none_post_bi,regio_post_bi,fictief_post_bi,land_post_bi,cont_post_bi,heelal_post_bi])
+	water_post_bi_un = get_unique_ne(water_post_bi, [lijn_post_bi,bc_post_bi,punt_post_bi,none_post_bi,regio_post_bi,fictief_post_bi,land_post_bi,cont_post_bi,heelal_post_bi])
+	none_post_bi_un = get_unique_ne(none_post_bi, [lijn_post_bi,bc_post_bi,water_post_bi,punt_post_bi,regio_post_bi,fictief_post_bi,land_post_bi,cont_post_bi,heelal_post_bi])
+	regio_post_bi_un = get_unique_ne(regio_post_bi, [lijn_post_bi,bc_post_bi,water_post_bi,none_post_bi,punt_post_bi,fictief_post_bi,land_post_bi,cont_post_bi,heelal_post_bi])
+	fictief_post_bi_un = get_unique_ne(fictief_post_bi, [lijn_post_bi,bc_post_bi,water_post_bi,none_post_bi,regio_post_bi,punt_post_bi,land_post_bi,cont_post_bi,heelal_post_bi])
+	land_post_bi_un = get_unique_ne(land_post_bi, [lijn_post_bi,bc_post_bi,water_post_bi,none_post_bi,regio_post_bi,fictief_post_bi,punt_post_bi,cont_post_bi,heelal_post_bi])
+	cont_post_bi_un = get_unique_ne(cont_post_bi, [lijn_post_bi,bc_post_bi,water_post_bi,none_post_bi,regio_post_bi,fictief_post_bi,land_post_bi,punt_post_bi,heelal_post_bi])
+	heelal_post_bi_un = get_unique_ne(heelal_post_bi, [lijn_post_bi,bc_post_bi,water_post_bi,none_post_bi,regio_post_bi,fictief_post_bi,land_post_bi,cont_post_bi,punt_post_bi])
+	
+	org_none_pre_bi_un = get_unique_ne(org_none_pre_bi, [org_misc_pre_bi,gov_pre_bi,com_pre_bi])
+	org_misc_pre_bi_un = get_unique_ne(org_misc_pre_bi, [org_none_pre_bi,gov_pre_bi,com_pre_bi])
+	gov_pre_bi_un = get_unique_ne(gov_pre_bi, [org_misc_pre_bi,org_none_pre_bi,com_pre_bi])
+	com_pre_bi_un = get_unique_ne(com_pre_bi, [org_misc_pre_bi,gov_pre_bi,org_none_pre_bi])
+	
+	org_none_post_bi_un = get_unique_ne(org_none_post_bi, [org_misc_post_bi,gov_post_bi,com_post_bi])
+	org_misc_post_bi_un = get_unique_ne(org_misc_post_bi, [org_none_post_bi,gov_post_bi,com_post_bi])
+	gov_post_bi_un = get_unique_ne(gov_post_bi, [org_misc_post_bi,org_none_post_bi,com_post_bi])
+	com_post_bi_un = get_unique_ne(com_post_bi, [org_misc_post_bi,gov_post_bi,org_none_post_bi])
+	
+	
+	# ORGANISATION DATA
+	
 	org_none= org_misc= gov= com = 0
 	org_noneNE=[]
 	org_miscNE =[]
 	govNE = []
 	comNE =[]
 	for item in org:
-		ne = ' '.join(item[2:-2]).lower().rstrip()
+		ne = item[2]
 		if item[-1] == org_subcats[0]:
 			org_none += 1
 			org_noneNE.append(ne)
@@ -92,7 +393,11 @@ def main():
 		elif item[-1] == org_subcats[3]:
 			com += 1
 			comNE.append(ne)
-
+	# GET UNIQUE LISTS OF WORDS PER SUBCAT
+	uniqueorg_none = get_unique_ne(org_noneNE, [org_miscNE,govNE,comNE])
+	uniqueorg_misc = get_unique_ne(org_miscNE, [org_noneNE,govNE,comNE])
+	uniquegov = get_unique_ne(govNE, [org_miscNE,org_noneNE,comNE])
+	uniquecom = get_unique_ne(comNE, [org_miscNE,govNE,org_noneNE])
 	
 	pro_subcats = ['AANDEEL', 'MISC', 'TAAL']
 	for item in pro:
@@ -134,9 +439,59 @@ def main():
 	print("ORG MISC: {}".format(Counter(org_miscNE).most_common(10)))
 	print("ORG GOV: {}".format(Counter(govNE).most_common(10)))
 	print("ORG COM: {}".format(Counter(comNE).most_common(10)))
+	print("\n")
+	print("Top ten words in each subcat AND ONLY IN THAT SUBCAT")
+	print("LOC PUNT: {}".format(Counter(uniquepunt).most_common(10)))
+	print("LOC LIJN: {}".format(Counter(uniquelijn).most_common(10)))
+	print("LOC BC: {}".format(Counter(uniquebc).most_common(10)))
+	print("LOC WATER: {}".format(Counter(uniquewater).most_common(10)))
+	print("LOC NONE: {}".format(Counter(uniquenone).most_common(10)))
+	print("LOC REGIO: {}".format(Counter(uniqueregio).most_common(10)))
+	print("LOC FICTIEF: {}".format(Counter(uniquefictief).most_common(10)))
+	print("LOC LAND: {}".format(Counter(uniqueland).most_common(10)))
+	print("LOC CONT: {}".format(Counter(uniquecont).most_common(10)))
+	print("LOC HEELAL: {}".format(Counter(uniqueheelal).most_common(10)))
+	print("\n")
+	print("ORG NONE: {}".format(Counter(uniqueorg_none).most_common(10)))
+	print("ORG MISC: {}".format(Counter(uniqueorg_misc).most_common(10)))
+	print("ORG GOV: {}".format(Counter(uniquegov).most_common(10)))
+	print("ORG COM: {}".format(Counter(uniquecom).most_common(10)))
+	print("\n")
+	print("Top ten unique word bigrams in front of NE")
+	print("LOC PUNT: {}".format(Counter(punt_pre_bi_un).most_common(10)))
+	print("LOC LIJN: {}".format(Counter(lijn_pre_bi_un).most_common(10)))
+	print("LOC BC: {}".format(Counter(bc_pre_bi_un).most_common(10)))
+	print("LOC WATER: {}".format(Counter(water_pre_bi_un).most_common(10)))
+	print("LOC NONE: {}".format(Counter(none_pre_bi_un).most_common(10)))
+	print("LOC REGIO: {}".format(Counter(regio_pre_bi_un).most_common(10)))
+	print("LOC FICTIEF: {}".format(Counter(fictief_pre_bi_un).most_common(10)))
+	print("LOC LAND: {}".format(Counter(land_pre_bi_un).most_common(10)))
+	print("LOC CONT: {}".format(Counter(cont_pre_bi_un).most_common(10)))
+	print("LOC HEELAL: {}".format(Counter(heelal_pre_bi_un).most_common(10)))
+	print("\n")
+	print("ORG NONE: {}".format(Counter(org_none_pre_bi_un).most_common(10)))
+	print("ORG MISC: {}".format(Counter(org_misc_pre_bi_un).most_common(10)))
+	print("ORG GOV: {}".format(Counter(gov_pre_bi_un).most_common(10)))
+	print("ORG COM: {}".format(Counter(com_pre_bi_un).most_common(10)))
+	print("\n")
+	print("Top ten unique word bigrams after NE")
+	print("LOC PUNT: {}".format(Counter(punt_post_bi_un).most_common(10)))
+	print("LOC LIJN: {}".format(Counter(lijn_post_bi_un).most_common(10)))
+	print("LOC BC: {}".format(Counter(bc_post_bi_un).most_common(10)))
+	print("LOC WATER: {}".format(Counter(water_post_bi_un).most_common(10)))
+	print("LOC NONE: {}".format(Counter(none_post_bi_un).most_common(10)))
+	print("LOC REGIO: {}".format(Counter(regio_post_bi_un).most_common(10)))
+	print("LOC FICTIEF: {}".format(Counter(fictief_post_bi_un).most_common(10)))
+	print("LOC LAND: {}".format(Counter(land_post_bi_un).most_common(10)))
+	print("LOC CONT: {}".format(Counter(cont_post_bi_un).most_common(10)))
+	print("LOC HEELAL: {}".format(Counter(heelal_post_bi_un).most_common(10)))
+	print("\n")
+	print("ORG NONE: {}".format(Counter(org_none_post_bi_un).most_common(10)))
+	print("ORG MISC: {}".format(Counter(org_misc_post_bi_un).most_common(10)))
+	print("ORG GOV: {}".format(Counter(gov_post_bi_un).most_common(10)))
+	print("ORG COM: {}".format(Counter(com_post_bi_un).most_common(10)))
 	
 	
-
 	
 if __name__ == '__main__':
 	main()
