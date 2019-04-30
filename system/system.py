d078152d02b24e02465179646e87e8bf426f4d1b
J.J.A. Schuitemaker <s2698617@lett-edu-145-117.let.rug.nl>
2019-04-30 16:24:31 +0200

    normalized feature names in insights and exported initial decision list as pickled dictionary, then I added the following aspects to the system : reading in the initial_DL, going through the development_data, getting the feature information around a found ne in order to start creating more features when data occurs in the initial decision list

--- system/system.py
+++ system/system.py
@@ -1,16 +1,123 @@
+import pickle
 
+# open the initial decision list made in insights.py
+def load_initial_DL():
+	pickle_in = open("../initial_DL/initial_DL.pkl","rb")
+	initial_DL = pickle.load(pickle_in)
+	return initial_DL
+
+def label_ne(unique,post_bi_un,pre_bi_un,post_si_un,pre_si_un, decision_list):
+	# label and grab features to temporary list
+	# step 1: check to see if feature occurs in the decision list
+	# when occurences are found take the label with the highest weight
+	# add all the features to the temporary decision list to the corresponding subcategory as feature
+	# weigh the features in the temprorary decision list
+	# filter the temporary decision list taking the top n weighted features
+	# add the top n features per subcategory to the initial decision list including weight
+	# label the data using the new decision list
+	# return the newly labeled data
+	return True
 
 def main():
+	# read in intitial decision list as dictionary
+	# key = CAT_subcat_featurename, value = list of features
+	initial_DL = load_initial_DL()
+	list_of_feature_keys = initial_DL.keys()
+	# add weight of 1 for initial decision list items
+	for key, value in initial_DL.items():
+		weight = float(1)
+		weighted_feats = []
+		for feature in value:
+			feature = tuple((feature, weight))
+			weighted_feats.append(feature)
+		initial_DL[key] = weighted_feats
+
+	# list of feature names
+	feat_names = ['post_bi_un', 'post_si_un', 'pre_si_un', 'pre_bi_un', 'unique']
+
 	# read in dataset
+	lines = []
 	infile = open("../data/using_development/SoNaR1_dev.txt", "r",encoding="utf8")
 	next(infile)
-
-	# read in starting decision list
+	for line in infile:
+		line = line.rstrip().split("\t")
+		if line[0] != "DOCUMENT":
+			lines.append(line)
 
 	# step1: iterate through data in search of NE
+	counter = 0
+	temporary_DL = defaultdict(list)
+	for i in range((len(lines)-4)):
+		line_one = lines[i]
+		line_two = lines[(i+1)]
+		line_three = lines[(i+2)]
+		line_four = lines[(i+3)]
+		line_five = lines[(i+4)]
+		
+			
+		# step2: when NE found try to label it using decision list
+		# when able to label ne save feature information in temporary decision list
+		if i == 0:
+			# do for the two three lines in the file, then switch to the 3rdline for information
+			if len(line_one) == 5 and line_one[0] != "SENTENCE":
+				main_cat = line_one[3]
+				if line_two[0] == 'SENTENCE':
+					line_two = ["XXXXXXXX","XXXXXXXX","XXXXXXXX"]
+					line_three = ["XXXXXXXX","XXXXXXXX","XXXXXXXX"]
+				if line_three[0] == 'SENTENCE':
+					line_three = ["XXXXXXXX","XXXXXXXX","XXXXXXXX"]
+				unique = line_one[2]
+				post_bi_un =  tuple((line_two[2], line_three[2]))
+				pre_bi_un = tuple(("XXXXXXXX", "XXXXXXXX"))
+				post_si_un = line_two[2]
+				pre_si_un = "XXXXXXXX"				
+				
+				# label the NE
+				retrieved_label = label_ne(unique,post_bi_un,pre_bi_un,post_si_un,pre_si_un, initial_DL)
+				
+		elif i == 1:
+			if len(line_two) == 5 and line_two[0] != "SENTENCE":
+				main_cat = line_two[3]
+				if line_three[0] == "SENTENCE":
+					line_three = ["XXXXXXXX","XXXXXXXX","XXXXXXXX"]
+					line_four = ["XXXXXXXX","XXXXXXXX","XXXXXXXX"]
+				if line_four[0] == "SENTENCE":
+					line_four = ["XXXXXXXX","XXXXXXXX","XXXXXXXX"]
 
-	# step2: when NE found try to label it using decision list
-	# when able to label ne save feature information in temporary decision list
+				unique = line_two[2]
+				post_bi_un =  tuple((line_three[2], line_four[2]))
+				pre_bi_un = tuple(("XXXXXXXX", line_one[2]))
+				post_si_un = line_four[2]
+				pre_si_un = line_one[2]
+
+				# label the NE
+				retrieved_label = label_ne(unique,post_bi_un,pre_bi_un,post_si_un,pre_si_un, initial_DL)
+				
+		# for the rest of the document
+		else:
+			if len(line_three) == 5 and line_three[0] != "SENTENCE":
+				main_cat = line_three[3]
+				if line_one[0] == "SENTENCE":
+					line_one = ["XXXXXXXX","XXXXXXXX","XXXXXXXX"]
+				if line_two[0] == "SENTENCE":
+					line_one = ["XXXXXXXX","XXXXXXXX","XXXXXXXX"]
+					line_two = ["XXXXXXXX","XXXXXXXX","XXXXXXXX"]
+				if line_four[0] == "SENTENCE":
+					line_four = ["XXXXXXXX","XXXXXXXX","XXXXXXXX"]
+					line_five = ["XXXXXXXX","XXXXXXXX","XXXXXXXX"]
+				if line_five[0] == "SENTENCE":
+					line_five = ["XXXXXXXX","XXXXXXXX","XXXXXXXX"]
+
+				unique = line_three[2]
+				post_bi_un =  tuple((line_four[2], line_five[2]))
+				pre_bi_un = tuple((line_one[2], line_two[2]))
+				post_si_un = line_four[2]
+				pre_si_un = line_two[2]
+				
+				# label the NE
+				retrieved_label = label_ne(unique,post_bi_un,pre_bi_un,post_si_un,pre_si_un, initial_DL)
+
+	
 	
 	# step 3: choose top features from temporary decision list
 	# add top features to the main decision list
@@ -24,4 +131,3 @@ def main():
 	
 if __name__ == '__main__':
 	main()
-
